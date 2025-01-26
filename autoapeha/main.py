5db751af9b7be8060b03ed060433061774b9a57f
dmitryobs <dmitrijs.levikins@gmail.com>
2025-01-26 15:49:42 +0200

    Added functionality for future redesign

--- autoapeha/main.py
+++ autoapeha/main.py
@@ -1,7 +1,14 @@
-from app.config_loader import load_config
-from app import start_app
+from autoapeha.app.app import start_app
+from autoapeha.app.config import Config
+from autoapeha.logger.logger import create_logger
 
 if __name__ == "__main__":
-    config = load_config()
-    start_app(config)
+
+    config = Config()
+
+    logger = create_logger("app", config.logger_config)
+
+    logger.info("Starting app")
+
+    start_app(logger,config)
 
---
+++ autoapeha/model/logger.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class LoggerConfig:
+    log_level: str
